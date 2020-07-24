0b2fc40e1e39ef99c6a06e0ebbc39b6a33734311
Joseph Maxwell <joseph.maxwell@hyland.com>
2020-07-24 10:59:57 -0400

    add files

---
+++ CypressProject/cypress/integration/hy_tech.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+context('Hy-Tech Camp', () => {
+
+  it('visits the sample website', () => {
+    cy.visit('https://www.saucedemo.com')
+  })
+})
---
+++ FollowAlong.md
@@ -0,0 +1,1 @@
+# Cypress Follow-Along Activity
